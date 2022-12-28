15cac08854e8c959835d31677b8f4456e576cfb2
duwilaja <apaaja.software@gmail.com>
2022-12-28 14:23:05 +0700

    otlet

--- outlets.php
+++ outlets.php
@@ -192,7 +192,7 @@ include 'inc.menu.php';
 										</div>
 										
 									</div>
-									<!--div class="form-group">
+									<div class="form-group">
 										<label class="col-md-2 control-label">PIC</label>
 										<div class="col-md-4">
 											<input type="text" class="form-control input-sm" name="pic" id="pic" placeholder="...">
@@ -212,7 +212,7 @@ include 'inc.menu.php';
 										<div class="col-md-4">
 											<input type="text" class="form-control input-sm" name="contact2" id="contact2" placeholder="...">
 										</div>
-									</div-->
+									</div>
 									
 								</div>
 							</div>
